cff4711c31fb75b528430501620242298c4761a5
June Park <junepark202012@gmail.com>
2024-03-20 23:17:58 +0900

    feat: implement shadcn-svelte

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,9 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: ['class'],
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  safelist: ['dark'],
   theme: {
     extend: {
       fontFamily: {
@@ -10,6 +12,67 @@ export default {
         serif: ['Lora Variable', 'Noto Serif KR', ...defaultTheme.fontFamily.serif],
       }
     },
+    colors: {
+      border: 'hsl(var(--border) / <alpha-value>)',
+      input: 'hsl(var(--input) / <alpha-value>)',
+      ring: 'hsl(var(--ring) / <alpha-value>)',
+      background: 'hsl(var(--background) / <alpha-value>)',
+      foreground: 'hsl(var(--foreground) / <alpha-value>)',
+      primary: {
+        DEFAULT: 'hsl(var(--primary) / <alpha-value>)',
+        foreground: 'hsl(var(--primary-foreground) / <alpha-value>)'
+      },
+      secondary: {
+        DEFAULT: 'hsl(var(--secondary) / <alpha-value>)',
+        foreground: 'hsl(var(--secondary-foreground) / <alpha-value>)'
+      },
+      destructive: {
+        DEFAULT: 'hsl(var(--destructive) / <alpha-value>)',
+        foreground: 'hsl(var(--destructive-foreground) / <alpha-value>)'
+      },
+      muted: {
+        DEFAULT: 'hsl(var(--muted) / <alpha-value>)',
+        foreground: 'hsl(var(--muted-foreground) / <alpha-value>)'
+      },
+      accent: {
+        DEFAULT: 'hsl(var(--accent) / <alpha-value>)',
+        foreground: 'hsl(var(--accent-foreground) / <alpha-value>)'
+      },
+      popover: {
+        DEFAULT: 'hsl(var(--popover) / <alpha-value>)',
+        foreground: 'hsl(var(--popover-foreground) / <alpha-value>)'
+      },
+      card: {
+        DEFAULT: 'hsl(var(--card) / <alpha-value>)',
+        foreground: 'hsl(var(--card-foreground) / <alpha-value>)'
+      }
+    },
+    borderRadius: {
+      lg: 'var(--radius)',
+      md: 'calc(var(--radius) - 2px)',
+      sm: 'calc(var(--radius) - 4px)'
+    },
+    typography: (/** @type {any} */ theme) => ({
+      DEFAULT: {
+        css: {
+          'blockquote p:first-of-type::before': {
+            content: ''
+          },
+          'blockquote p:last-of-type::after': {
+            content: ''
+          },
+          pre: {
+            outline: 'solid',
+            outlineWidth: '1px',
+            outlineColor: 'hsl(var(--border) / 1)',
+            paddingTop: '0.75rem',
+            paddingRight: '0.75rem',
+            paddingBottom: '0.75rem',
+            paddingLeft: '0.75rem'
+          }
+        }
+      }
+    })
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography')],
 };
