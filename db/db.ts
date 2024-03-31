f3ca557b5013fdbc298a00d59304df6df5683a60
June Park <junepark202012@gmail.com>
2024-04-01 06:12:48 +0900

    feat: set up local db env

--- db/db.ts
+++ db/db.ts
@@ -1,9 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import "dotenv/config";
 import { Kysely } from "kysely";
 import { LibsqlDialect } from "@libsql/kysely-libsql";
 
-export const DB_URL = process.env.DATABASE_URL === "dev" ? process.env.DATABASE_URL : process.env.DEV_DATABASE_URL;
-export const DB_AUTH_TOKEN = process.env.DATABASE_URL === "dev" ? process.env.DATABASE_AUTH_TOKEN : undefined;
+export const DB_URL = process.env.DEV_ENV === "true" ?  process.env.DEV_DATABASE_URL as string : process.env.DATABASE_URL as string
+export const DB_AUTH_TOKEN = process.env.DEV_ENV === "true" ?  undefined : process.env.DATABASE_AUTH_TOKEN
 
 export const db = new Kysely<any>({
   dialect: new LibsqlDialect({
