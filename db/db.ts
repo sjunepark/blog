992fbf280efc2cd54bfcc04dd4416a372324a05f
June Park <junepark202012@gmail.com>
2024-04-01 06:06:04 +0900

    refactor: change db env variable

--- db/db.ts
+++ db/db.ts
@@ -2,11 +2,12 @@ import "dotenv/config";
 import { Kysely } from "kysely";
 import { LibsqlDialect } from "@libsql/kysely-libsql";
 
-interface Database {}
+export const DB_URL = process.env.DATABASE_URL === "dev" ? process.env.DATABASE_URL : process.env.DEV_DATABASE_URL;
+export const DB_AUTH_TOKEN = process.env.DATABASE_URL === "dev" ? process.env.DATABASE_AUTH_TOKEN : undefined;
 
-export const db = new Kysely<Database>({
+export const db = new Kysely<any>({
   dialect: new LibsqlDialect({
-    url: process.env.DATABASE_ONLY_URL,
-    authToken: process.env.DATABASE_ONLY_AUTH_TOKEN,
+    url: DB_URL,
+    authToken: DB_AUTH_TOKEN
   }),
 });
