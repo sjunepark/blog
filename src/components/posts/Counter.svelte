0cce6a23818cc3e419da249910fc76ab1f4e65bd
June Park <junepark202012@gmail.com>
2024-04-01 21:27:09 +0900

    feat: implement basic counter

---
+++ src/components/posts/Counter.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import type { PostResponse } from "@pages/api/posts/[postId]";
+
+  export let postId: number;
+
+  onMount(async () => {
+    console.log("Hi");
+    // const postData = (await fetch(`/api/posts/${postId}`, {
+    //   method: "GET",
+    // })
+    //   .then((res) => res.json())
+    //   .catch((err) => {
+    //     return { error: err.message };
+    //   })) as PostResponse;
+    //
+    // if (postData.error) {
+    //   console.error(postData.error);
+    //   return;
+    // }
+  });
+</script>
+
+<div></div>
