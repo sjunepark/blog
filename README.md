ba13e0a12c63b733f8a78aa3d2ae6ce6dc843aee
June Park <junepark202012@gmail.com>
2024-04-06 16:54:55 +0900

    temp

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 - [ ] Filter only published posts'
   - [link](https://akashrajpurohit.com/blog/set-up-draft-pages-effectively-in-astro-with-configdriven-content-authoring/)
+
+## Enhancements
+- [ ] Enhance api source code
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
-import { defineConfig } from "astro/config";
+import {defineConfig} from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import {
+  transformerNotationDiff,
   transformerNotationHighlight,
   transformerNotationWordHighlight,
-  transformerNotationDiff,
 } from "@shikijs/transformers";
-
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
